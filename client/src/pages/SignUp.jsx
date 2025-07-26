2cd4a2c381aebde30968d1a7da7c3eea415acfec
raghav <raghavgoel0212@gmail.com>
2025-07-26 16:06:35 +0530

    signup reqs ui added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -13,6 +13,11 @@ import { Link } from "react-router-dom";
 import { FaCheckCircle } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { BsExclamationCircle } from "react-icons/bs";
+import { FaCross } from "react-icons/fa6";
+import { RiCrossLine } from "react-icons/ri";
+import { GiCrossMark } from "react-icons/gi";
+import { MdDone } from "react-icons/md";
 
 export default function Login() {
   const [formData, setFormData] = useState({
@@ -23,7 +28,12 @@ export default function Login() {
     password: "",
     nationality: "",
   });
-
+  const [eligible, setEligible] = useState({
+    dob: false,
+    phoneNo: false,
+    aadharNo: false,
+    password: false,
+  });
   const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [vIDShow, setVIDShow] = useState("");
@@ -31,11 +41,33 @@ export default function Login() {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
+    const { id, value } = e.target;
+    const updatedFormData = { ...formData, [id]: value };
+    setFormData(updatedFormData);
+
+    const birthDate = new Date(updatedFormData.dob);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+
+    setEligible({
+      dob: age >= 18,
+      phoneNo: /^\d{10}$/.test(updatedFormData.phoneNo.trim()),
+      aadharNo: /^\d{12}$/.test(updatedFormData.aadharNo.trim()),
+      password: updatedFormData.password.length >= 8,
     });
   };
+
+  console.log(eligible.dob);
+  console.log(eligible.phoneNo);
+  console.log(eligible.aadharNo);
+  console.log(eligible.password);
 
   const handleSignup = async (e) => {
     e.preventDefault();
@@ -77,14 +109,14 @@ export default function Login() {
           className="size-full shadow-sm shadow-blue-500"
         />
       </div>
-      <div className="text-center lg:mr-20">
-        <div className="font-medium text-4xl mb-10 mt-12 capitalize flex flex-col">
+      <div className="lg:mr-20">
+        <div className="text-center font-medium text-4xl mb-10 mt-12 capitalize flex flex-col">
           <span>Welcome to EVkendra</span>
           <span className="text-blue-600 italic">create your account</span>
         </div>
         <form
           onSubmit={handleSignup}
-          className="flex flex-col gap-4 mt-4 items-center sm:items-start"
+          className="flex flex-col gap-4 mt-4 items-center sm:items-start text-center"
         >
           <div className="sm:flex sm:space-x-24">
             <div className="sm:mt-2 sm:mr-0.5">
@@ -207,7 +239,7 @@ export default function Login() {
             gradientDuoTone="purpleToBlue"
             type="submit"
             disabled={loading}
-            className="w-[97%]"
+            className="w-[100%]"
           >
             {loading ? (
               <>
@@ -219,6 +251,75 @@ export default function Login() {
             )}
           </Button>
         </form>
+        <div
+          className={`p-4 mt-3 rounded-md ${
+            eligible.dob &&
+            eligible.aadharNo &&
+            eligible.phoneNo &&
+            eligible.password
+              ? "bg-green-200"
+              : "bg-red-200"
+          }`}
+        >
+          <div className={`flex flex-row gap-2  text-red-600 items-center`}>
+            <BsExclamationCircle className=" size-5 mt-0.5" />
+            <div className="capitalize font-semibold text-xl">Requirements</div>
+          </div>
+          <div className="mt-5 font-medium flex flex-col ">
+            {eligible.dob ? (
+              <div className="flex flex-row items-center gap-3 text-green-600">
+                <MdDone className="mt-0.5" />
+                <span className="text-[19px]">Age 18+</span>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center gap-3 text-red-600">
+                <GiCrossMark className="mt-0.5" />
+                <span className="text-[19px]">Age 18+</span>
+              </div>
+            )}
+            {eligible.phoneNo ? (
+              <div className="flex flex-row items-center gap-3 text-green-600">
+                <MdDone className="mt-0.5" />
+                <span className="text-[19px]">
+                  Phone no. must be of 10 digits
+                </span>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center gap-3 text-red-600">
+                <GiCrossMark className="mt-0.5" />
+                <span className="text-[19px]">
+                  Phone no. must be of 10 digits
+                </span>
+              </div>
+            )}
+            {eligible.aadharNo ? (
+              <div className="flex flex-row items-center gap-3 text-green-600">
+                <MdDone className="mt-0.5" />
+                <span className="text-[19px]">
+                  Aadhar no. must be of 12 digits
+                </span>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center gap-3 text-red-600">
+                <GiCrossMark className="mt-0.5" />
+                <span className="text-[19px]">
+                  Aadhar no. must be of 12 digits
+                </span>
+              </div>
+            )}
+            {eligible.password ? (
+              <div className="flex flex-row items-center gap-3 text-green-600">
+                <MdDone className="mt-0.5" />
+                <span className="text-[19px]">Password length must be 8+</span>
+              </div>
+            ) : (
+              <div className="flex flex-row items-center gap-3 text-red-600">
+                <GiCrossMark className="mt-0.5" />
+                <span className="text-[19px]">Password length must be 8+</span>
+              </div>
+            )}
+          </div>
+        </div>
         <div className="flex flex-row justify-center mt-3 mb-10">
           <h1>Have an account...?</h1>
           <div className="text-blue-400 hover:text-blue-700 ml-1">
